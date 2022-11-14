c2969b336d242c68250b72368f710e403dcc97ca
小涛干饭 <9682800+xiaotao-dry-rice@user.noreply.gitee.com>
2022-11-14 11:09:13 +0800

    '更新初始化'

---
+++ unpackage/dist/dev/mp-weixin/app.js
@@ -0,0 +1,29 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports[Symbol.toStringTag] = "Module";
+var common_vendor = require("./common/vendor.js");
+if (!Math) {
+  "./pages/home/home.js";
+  "./pages/release/release.js";
+  "./pages/my/my.js";
+}
+const _sfc_main = {
+  onLaunch: function() {
+    console.log("App Launch");
+  },
+  onShow: function() {
+    console.log("App Show");
+  },
+  onHide: function() {
+    console.log("App Hide");
+  }
+};
+var App = /* @__PURE__ */ common_vendor._export_sfc(_sfc_main, [["__file", "D:/\u5FAE\u4FE1\u5C0F\u7A0B\u5E8F/miyoushe/App.vue"]]);
+function createApp() {
+  const app = common_vendor.createSSRApp(App);
+  return {
+    app
+  };
+}
+createApp().app.mount("#app");
+exports.createApp = createApp;
---
+++ unpackage/dist/dev/mp-weixin/pages/my/my.js
@@ -0,0 +1,21 @@
+"use strict";
+var common_vendor = require("../../common/vendor.js");
+const _sfc_main = {
+  __name: "my",
+  setup(__props) {
+    common_vendor.onLoad((message) => {
+    });
+    common_vendor.onShow(() => {
+    });
+    common_vendor.onHide(() => {
+    });
+    common_vendor.onShareAppMessage(() => {
+    });
+    return (_ctx, _cache) => {
+      return {};
+    };
+  }
+};
+var MiniProgramPage = /* @__PURE__ */ common_vendor._export_sfc(_sfc_main, [["__file", "D:/\u5FAE\u4FE1\u5C0F\u7A0B\u5E8F/miyoushe/pages/my/my.vue"]]);
+_sfc_main.__runtimeHooks = 2;
+wx.createPage(MiniProgramPage);
